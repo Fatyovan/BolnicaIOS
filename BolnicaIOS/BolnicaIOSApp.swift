e7fe0c739569df60ba965a83b87e8477d57ee85a
Ivan Jovanovik <Ivan.jovanovik@vpar.com>
2024-03-14 17:58:57 +0100

    Initial Commit

---
+++ BolnicaIOS/BolnicaIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BolnicaIOSApp.swift
+//  BolnicaIOS
+//
+//  Created by Ivan Jovanovik on 14/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct BolnicaIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
